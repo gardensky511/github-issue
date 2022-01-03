3043949587ff5ffda9ec0cc2acd4983e45b7a280
jeongmin <luvmini511@gmail.com>
2022-01-03 19:45:48 +0900

    chore: redux toolkit セットアップ

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import { ChakraProvider } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
 import App from './App';
+import { store } from './redux/store';
 
 ReactDOM.render(
   <StrictMode>
     <ChakraProvider>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </ChakraProvider>
   </StrictMode>,
   document.getElementById('root'),
