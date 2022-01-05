840ded9124c608d9c41982c4614e4fcf463818cc
jeongmin <luvmini511@gmail.com>
2022-01-05 23:18:04 +0900

    feat: タイトル・検索エリアUI実装

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,45 @@
+import styled from '@emotion/styled';
+import { Button, Input } from '@chakra-ui/react';
+
 export const Home = () => {
-  return <div>HOME</div>;
+  // todo: コンポーネントに分離
+  // todo: イベントハンドラー実装
+  return (
+    <Container>
+      <Title>🔍 Let&apos;s Search Issues 🔍</Title>
+      <SearchArea>
+        <Input placeholder="Enter the github ID (e.g facebook)" size="lg" />
+        <Slash />
+        <Input placeholder="Repository name, too (e.g react)" size="lg" />
+        <Button ml="10px" size="lg">
+          検索
+        </Button>
+      </SearchArea>
+    </Container>
+  );
 };
+
+const Container = styled.div`
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  text-align: center;
+  font-weight: 900;
+  margin-bottom: 30px;
+`;
+
+const SearchArea = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const Slash = styled.div`
+  width: 10px;
+  background-color: #a0afb8;
+  margin: 5px 10px;
+  transform: rotate(10deg);
+`;
