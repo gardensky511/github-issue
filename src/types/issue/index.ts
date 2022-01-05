988cf7ec6e2ad986663bd17dcf2e8912aad50191
jeongmin <luvmini511@gmail.com>
2022-01-05 23:45:03 +0900

    feat: issueにかかわる型追加

---
+++ src/types/issue/index.ts
@@ -0,0 +1,24 @@
+export type IssueCreator = {
+  login: string;
+  avatar_url: string;
+};
+
+export type Label = {
+  id: number;
+  name: string;
+  color: string;
+};
+
+export type IssueState = 'open' | 'closed';
+
+export type Issue = {
+  id: number;
+  title: string;
+  number: number;
+  user: IssueCreator;
+  body: string;
+  labels: Label[];
+  comments_url: string;
+  state: IssueState;
+  comments: number;
+};
