5427782118ac13d360b38b274f18931d8b317002
jeongmin <luvmini511@gmail.com>
2022-01-05 23:23:20 +0900

    feat: useAppDispatch追加

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 
 export const store = configureStore({
   reducer: {},
@@ -6,3 +7,5 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const useAppDispatch = () => useDispatch<AppDispatch>();
