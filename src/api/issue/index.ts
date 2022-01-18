ab52f53202739c2f7f9868a06c6af11f064f37f2
jeongmin <luvmini511@gmail.com>
2022-01-18 21:33:52 +0900

    refactor: fetch関数にasync-await 追加

--- src/api/issue/index.ts
+++ src/api/issue/index.ts
@@ -2,6 +2,6 @@ import { AXIOS, END_POINT } from '../const';
 import { InputValues } from '../../pages/Home/types';
 import { Issue } from '../../types/issue';
 
-export const getIssueList = (inputValues: InputValues) => {
-  return AXIOS.get<Issue[]>(END_POINT.GET_ISSUE_LIST(inputValues));
+export const getIssueList = async (inputValues: InputValues) => {
+  return await AXIOS.get<Issue[]>(END_POINT.GET_ISSUE_LIST(inputValues));
 };
--- src/api/repository/index.ts
+++ src/api/repository/index.ts
@@ -1,6 +1,6 @@
 import { AXIOS, END_POINT } from '../const';
 import { GetRepositoryDataParam, RepositoryData } from '../../types/repository';
 
-export const getRepositoryData = (getRepositoryDataParam: GetRepositoryDataParam) => {
-  return AXIOS.get<RepositoryData>(END_POINT.GET_REPOSITORY_DATA(getRepositoryDataParam));
+export const getRepositoryData = async (getRepositoryDataParam: GetRepositoryDataParam) => {
+  return await AXIOS.get<RepositoryData>(END_POINT.GET_REPOSITORY_DATA(getRepositoryDataParam));
 };
